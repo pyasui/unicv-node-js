10c3c94b145456f966be89d37e4a89dc3991b24a
pedroyasui <pedro.yasui@bitzen.com.br>
2025-03-12 17:00:01 -0300

    start implantação do swagger

--- 03-hello-world-swagger/index.js
+++ 03-hello-world-swagger/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const swaggerSetup = require('./swagger');
 const app = express();
-const port = 3000;
-
-// swagger
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger'); 
+const port = 3001;
 
 // configs
 app.use(express.json());
+
+swaggerSetup(app);
 
 // importar controllers
 const userRoutes = require('./routes/user-routes');
@@ -16,8 +15,6 @@ const productRoutes = require('./routes/product-routes');
 // routes
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
--- 03-hello-world-swagger/swagger.js
+++ 03-hello-world-swagger/swagger.js
@@ -1,15 +1,19 @@
 const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Gestão de Combustíveis',
+      title: 'API - Unicv',
       version: '1.0.0',
-      description: 'UNICV - API para gestão de combustíveis',
     },
   },
-  apis: ['./routes/*.js', './controllers/*.js'],
+  apis: ['./routes/*.js'], // Caminho para os arquivos de rotas
 };
 
-module.exports = swaggerJsdoc(options);
+const specs = swaggerJsdoc(options);
+
+module.exports = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+};
