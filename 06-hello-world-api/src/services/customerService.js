9e4e46eb879dbf924565c60cc0559b5c33886129
pedroyasui <pedro.yasui@bitzen.com.br>
2025-05-15 00:05:16 -0300

    ;

---
+++ 06-hello-world-api/src/services/customerService.js
@@ -0,0 +1,34 @@
+const api = require("./externalApiService");
+
+// Buscar todos os usuários
+async function getCustomers(limit, offset, name) {
+  try {
+    const response = await api.get('/v3/customers', {
+      params: {
+        limit: limit|| 10,
+        offset: offset || 0,
+        name: name
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao buscar os clientes:', error.message);
+    throw error;
+  }
+}
+
+// // Buscar um usuário por ID
+// async function getUserById(id) {
+//   try {
+//     const response = await api.get(`/users/${id}`);
+//     return response.data;
+//   } catch (error) {
+//     console.error(`Erro ao buscar usuário ${id}:`, error.message);
+//     throw error;
+//   }
+// }
+
+module.exports = {
+  getCustomers,
+  // getUserById
+};
